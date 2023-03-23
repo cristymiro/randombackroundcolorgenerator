349e22da4682d297dcce7f462e1742d71df8c23c
Cris <mirocristy@gmail.com>
2023-03-23 16:20:46 +0100

    second commit

---
+++ build/main.js
@@ -0,0 +1,15 @@
+"use strict";
+const button = document.querySelector(".clickButton");
+button.addEventListener("click", function (e) {
+    e.preventDefault();
+    const red = generateBgColor();
+    const green = generateBgColor();
+    const blue = generateBgColor();
+    const rgb = `${red}, ${green}, ${blue}`;
+    const backgrounColor = "rgb(" + rgb + ")";
+    document.body.style.background = backgrounColor;
+});
+function generateBgColor() {
+    const numberRandom = Math.floor(Math.random() * 256);
+    return numberRandom;
+}
